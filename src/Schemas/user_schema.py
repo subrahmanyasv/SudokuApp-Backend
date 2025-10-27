8db4589f275a5f3321253bd4640a3f3d4be977d1
chandradiproy <official.chandradip@gmail.com>
2025-10-27 06:38:56 +0530

    feat: included total challenges played and total challenges played by the user

--- src/Schemas/user_schema.py
+++ src/Schemas/user_schema.py
@@ -14,6 +14,12 @@ class UserData(UserBase):
     best_score_easy: int
     best_score_medium: int
     best_score_hard: int
+    # *** ADDED: Challenge Stats ***
+    total_challenges_played: int = 0
+    total_challenges_won: int = 0
+
+    class Config:
+        from_attributes = True # Ensure this is present
 
 class User(UserData):
     id: Optional[UUID]
